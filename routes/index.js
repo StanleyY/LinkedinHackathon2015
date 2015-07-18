5b430fc62ea4c62fbf3824ff1c1c8c3ee0fac494
Joanna Wang <joanna.j.wang@gmail.com>
2015-07-17 23:36:44 -0700

    Crappy landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Food Cheezus',
+                        slogan: 'Pray and you shall eat.',
+                        buttonText: 'Plan your next meal' });
+});
+
+router.get('/vote', function(req, res, next) {
+  res.render('vote', { title: 'Vote!!!' });
 });
 
 module.exports = router;
