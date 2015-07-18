55db868d89b8d7983335d44bc143eebafd0a3c14
Edgar Wang <edwang@edwang-mn1.linkedin.biz>
2015-07-18 02:34:35 -0700

    updaates

--- public/js/preferences.js
+++ public/js/preferences.js
@@ -5,8 +5,36 @@ var questions = [
 
 
 ];
+var cuisines = [
+"French",
+"Italian",
+"Chinese",
+"Indian",
+"Thai",
+"Mexican",
+"Japanese",
+"Spanish",
+"Greek",
+"Lebanese"
+];
+var allergies = [
+"eggs",
+"fish",
+"milk",
+"nuts",
+"shellfish",
+"soya",
+"wheat"
+];
+var prices = [
+"$",
+"$$",
+"$$$",
+"$$$$"
+]
 var app = angular.module('foodcheezus', []).
 controller('preferences', function($scope) {
+  $scope.answers = cuisines;
   $scope.counter = 0;
   $scope.question = questions[$scope.counter]
   $scope.next = function(){
@@ -16,6 +44,15 @@ controller('preferences', function($scope) {
     else {
       $scope.counter++;
       $scope.question = questions[$scope.counter];
+      if ($scope.counter == 1){
+        $scope.answers = allergies;
+      }
+      else if ($scope.counter == 2){
+        $scope.answers = prices;
+      }
+      else {
+        $scope.answers = [];
+      }
     }
   }
   $scope.messages = "MESSAGES NOW";
