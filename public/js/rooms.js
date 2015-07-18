c99141a690272889d3f4d122e7a6e3521397f771
Wei En Lee <welee@linkedin.com>
2015-07-18 10:49:59 -0700

    scraping sensitive data from NSA servers

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -42,3 +42,16 @@ controller('rooms', function($scope) {
   $scope.restaurants = restaurants;
 
 });
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+function showPosition(position) {
+     console.log("Latitude: " + position.coords.latitude + "\n" + "Longitude: " + position.coords.longitude);
+}
+
+getLocation();
